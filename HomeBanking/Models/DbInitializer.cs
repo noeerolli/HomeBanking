bfab527f7b405989c7d1b2f72ebb58bb744baa67
noeerolli <noeliarolli82@gmail.com>
2023-07-25 11:22:27 -0300

    Add project files.

---
+++ HomeBanking/Models/DbInitializer.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using System.Runtime.Serialization;
+
+namespace HomeBanking.Models
+{
+    public class DbInitializer
+    {
+        public static void Initialize(HomeBankingContext context) 
+        {
+            if (!context.Clients.Any())
+            {
+                var clients = new Client[]
+                {
+                    new Client  {
+                        FirstName = "Noe",
+                        LastName = "Rolli",
+                        Email = "blabla@gmail.com",
+                        Password = "12345"
+                    },
+                    new Client  {
+                        FirstName = "Pepe",
+                        LastName = "Lopez",
+                        Email = "blabl2a@gmail.com",
+                        Password = "123456"
+                    }
+
+                };
+
+                foreach (Client client in clients)
+                {
+                    context.Clients.Add(client);
+                }
+
+                
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
